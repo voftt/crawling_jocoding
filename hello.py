dce2c9a654bd2cf55ef78032bf22b16d11097e74
voftt1 <voftt1@gmail.com>
2022-01-22 01:48:45 +0900

    2

--- hello.py
+++ hello.py
@@ -1,1 +1,63 @@
-print("Hello, World!")
+# print("Hello, World!") 
+
+# a=3
+# b=4
+
+# print(a%b)
+
+# a = """Life is too short 
+# You
+#  need 
+#  python"""
+
+# print(a)
+
+# a = "Python"
+# b = " is fun!"
+# print(a * 100)
+
+# Indexing
+# a = "Life is too short, You need python"
+
+# print(a[2])
+# print(a[-2])
+
+
+# #Slicing
+# print(a[0:4])
+# a="20220122cloudy"
+# print(a[::-2])
+# # [이상:미만:간격] 슬라이싱
+
+##Formating
+# a = "i eat %d apples." % 3 
+# b = "i eat " + str(3) + " apples."
+# print(a) 
+# print(b)
+
+# number = 10
+# day = "three"
+# a = "i ate %d apples. so i was sick for %s days."% (number, day)
+# print(a)
+
+# a = "%10s" % "hi"
+
+# print(a)
+
+# a = "%0.4f" % 3.412423214
+
+# print(a)
+
+# # 함수
+# a = "hobby"
+# print(a.count("b")) 
+# print(a.find("b"))
+# print(a.find("x"))
+
+# a = ",".join("abcd")
+
+# print(a)
+
+a = "Life:is:too:short"
+print(a.replace("Life", "Your neck"))
+print(a.split(":"))
